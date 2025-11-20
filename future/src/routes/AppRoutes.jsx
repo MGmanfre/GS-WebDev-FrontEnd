45b57745c86c276161e7c05fd7923a6f1a149594
MGmanfre <miguelmanfre0603@gmail.com>
2025-11-20 20:15:21 -0300

    fix: appRoutes for the other pages

--- future/src/routes/AppRoutes.jsx
+++ future/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Layout from '../Layout.jsx'
 import Home from '@/pages/Home.jsx'
 import About from '@/pages/About.jsx'
 import Contact from '@/pages/Contact.jsx'
+import Dashboard from '@/pages/Dashboard.jsx'
 
 export const router = createBrowserRouter([
     {
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
             {index: true, element: <Home/>},
             {path: 'about', element: <About/>},
             {path: 'contact', element: <Contact/>},
+            {path: 'dashboard', element: <Dashboard/>},
         ],
     },
 ])
