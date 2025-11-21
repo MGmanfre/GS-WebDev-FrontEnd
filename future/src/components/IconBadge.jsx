f425b14f2d5cc0f7a7cb52eae0f79b7d6f1b7a95
MGmanfre <miguelmanfre0603@gmail.com>
2025-11-20 21:18:21 -0300

    fix

--- future/src/components/IconBadge.jsx
+++ future/src/components/IconBadge.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function IconBadge({ icon, textClass = "text-blue-400", bgClass = 'bg-blue-700', size = 'h-16 w-16' }) {
+export default function IconBadge({ icon, textClass = "text-white", bgClass = 'bg-blue-700', size = 'h-16 w-16' }) {
   return (
     <div className={`${size} inline-flex items-center justify-center ${bgClass} ${textClass} rounded-xl shadow-md`} aria-hidden="true">
       {React.isValidElement(icon) ? React.cloneElement(icon, { className: 'w-6 h-6' }) : icon}
