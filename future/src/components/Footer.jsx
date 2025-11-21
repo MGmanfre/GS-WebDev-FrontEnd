ba452775613376798e69931d53eb1abb862afe2f
MGmanfre <miguelmanfre0603@gmail.com>
2025-11-20 23:02:23 -0300

    fix: footer

--- future/src/components/Footer.jsx
+++ future/src/components/Footer.jsx
@@ -11,11 +11,11 @@ export default function Footer() {
                 <div className="flex flex-col md:flex-row items-stretch justify-center gap-24 py-4">
                     <div className="bg-zinc-700 shadow-black rounded-2xl flex flex-col items-center p-4 w-full md:w-72">
                         <img src={footerLogo} alt="Logo" className="h-20 w-auto" />
-                        <p className="text-sm mt-2 text-center">Conectando talentos ao futuro do trabalho</p>
+                        <p className="text-lg mt-2 text-center">Conectando talentos ao futuro do trabalho</p>
                     </div>
 
                     <div className="bg-zinc-700 shadow-black rounded-2xl flex flex-col items-center p-4 w-full md:w-96">
-                        <h4 className="font-bold text-white mb-4">Geral</h4>
+                        <h4 className="text-2xl font-bold text-white mb-4">Geral</h4>
                         <ul className="flex flex-col space-y-2 text-xl text-center">
                             <li><Link to="/about" className="hover:text-black transition-colors duration-200">Sobre Nós</Link></li>
                             <li><Link to="/contact" className="hover:text-black transition-colors duration-200">Contato</Link></li>
@@ -24,7 +24,7 @@ export default function Footer() {
                     </div>
 
                     <div className="bg-zinc-700 shadow-black rounded-2xl flex flex-col items-center p-4 w-full md:w-72">
-                        <h4 className="font-bold text-white mb-4">Redes Sociais</h4>
+                        <h4 className="text-2xl font-bold text-white mb-4">Redes Sociais</h4>
                         <ul className="flex flex-col items-center gap-2 justify-center space-x-4 text-2xl">
                             <li><a href="#" className="hover:text-black transition-colors duration-200 flex flex-row gap-1 items-center"><SiInstagram/>Instagram</a></li>
                             <li><a href="#" className="hover:text-black transition-colors duration-200 flex flex-row gap-1 items-center"><BsTwitterX/>Twitter</a></li>
